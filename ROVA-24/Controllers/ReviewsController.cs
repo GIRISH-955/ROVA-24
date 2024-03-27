29ed18168d62ac9af17080e39895d310ba3b7a85
All <All@DESKTOP-B1U473I>
2024-03-27 11:55:55 +0530

    Added apis for adding , getting and deleting the reviews

--- ROVA-24/Controllers/ReviewsController.cs
+++ ROVA-24/Controllers/ReviewsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ROVA_24.DTO.ReviewsDTO;
 using ROVA_24.IServices;
+using ROVA_24.Services;
 
 namespace ROVA_24.Controllers
 {
@@ -25,6 +26,37 @@ namespace ROVA_24.Controllers
             if(Result.Success && Result.Message== "Error occured while adding reviews")
                 return BadRequest(Result);
             return BadRequest(Result);
+        }
+        [EnableCors("CORSPolicy")]
+        [HttpGet("getAllReviews")]
+        public async Task<IActionResult> getAllReviews()
+        {
+            var result = await _reviewsServices.GetAllReviewsAsync();
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [EnableCors("CORSPolicy")]
+        [HttpGet("getReviews{reviewId}")]
+        public async Task<IActionResult> getReviewsById(int reviewId)
+        {
+            var result = await _reviewsServices.getReviewsByIdAsync(reviewId);
+            if (result.Success)
+                return Ok(result);
+            if (result.Success && result.Message == "Address not found")
+                return BadRequest(result);
+            return BadRequest(result);
+        }
+        [EnableCors("CORSPolicy")]
+        [HttpDelete("DeleteReviews")]
+        public async Task<IActionResult> deleteReviewsById(int reviewId)
+        {
+            var result = await _reviewsServices.deleteReviewsByIdAsync(reviewId);
+            if(result.Success)
+                return Ok(result);
+            if (result.Success && result.Message == "Reviews Does not exist")
+                return BadRequest(result);
+            return BadRequest(result);
         }
     }
 }
